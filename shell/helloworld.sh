278a95e3673f70e956c64afe45e3ae2b1d808083
ones1kk <kwsic@naver.com>
2022-01-22 17:10:00 +0900

    ✨ Add : Variable1

--- shell/helloworld.sh
+++ shell/helloworld.sh
@@ -2,3 +2,9 @@
 
 echo hello world
 
+
+TEST="hello world"
+echo $TEST
+
+TEST_=hello
+echo $TEST_
---
+++ shell/var.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+hello="ppppppppp"
+Hello="aaaaaa"
+HELLO="AAAAAA"
+
+echo $HELLO
+echo ${Hello}
+echo ${hello}
+
+E=aaaa
+EE=ee
+EEE=eee
+
+echo ${E}
+echo ${EE}
+echo ${EEE}
+echo "$E EE"
+echo ${E} EE
+echo hi
+
