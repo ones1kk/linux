3f3d95a3bc2528d43adff413e90b593330cdc527
ones1kk <kwsic@naver.com>
2022-02-12 01:07:37 +0900

    🔨 Update : break & continue

--- shell/while.sh
+++ shell/while.sh
@@ -3,16 +3,28 @@
 index=0
 while [ $index -lt 10 ]
 do
-	if [ $index -eq 5 ]; then
-		echo "bye bye"
-		break
-		#continue
-	fi
+	#if [ $index -eq 5 ]; then
+	#	echo "bye bye"
+	#	break
+	#	#continue
+	#fi
 
-	let 'index++'
+	while [ .. ]
+	do
+		if[ ... ]; then
+			break
+		fi
+	done
+
+	#let 'index++'
 	echo "index is $index"
+	((index++))
+	echo "now index is $index"
 	
 done
+echo "bye"
+
+#break, continue는 가장 근접한 반복문에 대해서 동작
 
 
 #index=0
